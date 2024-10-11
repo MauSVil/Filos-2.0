42e71ea84af47feab793de4747ff629799b9d11b
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2024-10-10 22:35:32 -0600

    test messages

---
+++ app/api/tests/messages/route.ts
@@ -0,0 +1,52 @@
+import clientPromise from "@/mongodb";
+import { Db } from "mongodb";
+import { NextRequest, NextResponse } from "next/server";
+import _ from "lodash";
+
+export const GET = async (req: NextRequest) => {
+  try {
+    const client = await clientPromise;
+    const db = client.db('test') as Db;
+
+    const whatsappMessagesCollection = await db.collection('whatsapp-messages');
+    const whatsappContactsCollection = await db.collection('whatsapp-contacts');
+
+    const whatsappMessages = await whatsappMessagesCollection.find({}).toArray();
+    const groupedMessages = _.groupBy(whatsappMessages, 'phone_id');
+
+    const contactsToDelete = [];
+    const messagesToDelete = [];
+
+    for (const phoneId in groupedMessages) {
+      const messages = groupedMessages[phoneId];
+      if (messages.length === 2) {
+        for (const message of messages) {
+          messagesToDelete.push(message._id);
+        }
+        contactsToDelete.push(phoneId);
+      }
+    }
+
+    console.log(contactsToDelete);
+    console.log(messagesToDelete);
+
+    // await whatsappContactsCollection.deleteMany({
+    //   phone_id: {
+    //     $in: contactsToDelete.map(contact => contact.toString()) 
+    //   }
+    // });
+
+    // await whatsappMessagesCollection.deleteMany({
+    //   _id: {
+    //     $in: messagesToDelete
+    //   }
+    // });
+
+    return NextResponse.json({ contactsToDelete, messagesToDelete });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+    return NextResponse.json({ error: 'An error occurred' }, { status: 500 });
+  }
+}

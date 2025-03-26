cfa9a5c5b27940c38c52aa733c7119bbfe184ef5
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2025-03-26 02:32:30 -0600

    fix?

--- types/RepositoryTypes/Product.client.ts
+++ types/RepositoryTypes/Product.client.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 import { ProductModel } from "./Product";
 
-export const ProductClient = ProductModel.and(z.object({
+export const ProductClient = ProductModel.extend({
   image: z.instanceof(File).or(z.string()).optional(),
-}))
+});
 
 export type ProductClient = z.infer<typeof ProductClient>;

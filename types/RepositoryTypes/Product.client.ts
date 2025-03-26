5ecf79894bfd524510d98b0db91078b47d885553
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2025-03-26 17:21:17 -0600

    fix?

--- types/RepositoryTypes/Product.client.ts
+++ types/RepositoryTypes/Product.client.ts
@@ -1,8 +1,12 @@
 import { z } from "zod";
 import { ProductInputModel } from "./Product";
 
+const isClient = typeof window !== "undefined";
+
 export const ProductInputClient = ProductInputModel.extend({
-  image: z.instanceof(File).or(z.string()).optional(),
+  image: isClient
+    ? z.instanceof(File).or(z.string()).optional()
+    : z.any().optional(),
 });
 
 export type ProductInputClient = z.infer<typeof ProductInputClient>;

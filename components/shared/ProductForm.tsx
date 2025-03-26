9600a043955872c5a66220d5e1e448461e68ca09
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2025-03-26 02:23:52 -0600

    small fix

--- components/shared/ProductForm.tsx
+++ components/shared/ProductForm.tsx
@@ -1,9 +1,9 @@
 import { UseFormReturn } from "react-hook-form"
 import { InputFormField } from "../form/InputFormField"
 import { ComboboxFormField } from "../form/ComboboxField"
-import { Button } from "@nextui-org/button"
 import { ProductClient } from "@/types/RepositoryTypes/Product.client";
 import { Form } from "../form";
+import { Button } from "../ui/button";
 
 interface Props {
   form: UseFormReturn<ProductClient>;

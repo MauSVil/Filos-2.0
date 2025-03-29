27688c485e20a9c3edaf6e91ac94fb98b2570887
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2025-03-29 17:57:00 -0600

    sign-in route

--- app/api/sign-in/route.ts
+++ app/api/sign-in/route.ts
@@ -1,43 +1,57 @@
 import { UsersRepository } from "@/repositories/users.repository";
-import ky from "ky";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
+import { z, ZodError } from "zod";
+import bycript from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const Body = z.object({
   email: z.string().email(),
-  password: z.string().min(8),
+  password: z.string().min(4),
 })
 
 type Body = z.infer<typeof Body>;
 
-export const POST = async (req: NextRequest) => {
-  let validatedBody: Body;
-
+export const POST = async (req: NextRequest) => {  
   try {
-    validatedBody = Body.parse(await req.json());
-  } catch (error) {
-    return NextResponse.json(
-      { message: "Error al iniciar sesion" },
-      { status: 400 },
-    );
-  }
-
-  try {
-    console.log({ validatedBody });
+    const validatedBody = Body.parse(await req.json());
     const userFound = await UsersRepository.findOne({ email: validatedBody.email });
-    console.log({ userFound });
 
-    // const json: { data: { token: string }; error: string } = await resp.json();
+    if (!userFound) throw new Error("Usuario no encontrado");
 
-    // if (json.error) throw new Error(json.error);
-    // (await cookies()).set("token", json?.data?.token, {
-    //   maxAge: 60 * 60 * 24 * 7,
-    // });
+    const isValid = await bycript.compare(validatedBody.password, userFound.password);
+    if (!isValid) throw new Error("Contraseña incorrecta");
+
+    const myUser = {
+      id: userFound._id,
+      name: userFound.name,
+      email: userFound.email,
+      role: userFound.role,
+    }
+
+    const token = await jwt.sign(myUser, 'secretWord');
+
+    (await cookies()).set("token", token, {
+      maxAge: 60 * 60 * 24 * 7,
+    });
 
     return NextResponse.json({ message: "Se inicio sesion correctamente" });
   } catch (error) {
     console.log(error, "error");
+
+    if (error instanceof ZodError) {
+      return NextResponse.json(
+        { message: error.issues[0].message },
+        { status: 400 },
+      );
+    }
+
+    if (error instanceof Error) {
+      return NextResponse.json(
+        { message: error.message },
+        { status: 400 },
+      );
+    }
 
     return NextResponse.json(
       { message: "Error al iniciar sesion" },

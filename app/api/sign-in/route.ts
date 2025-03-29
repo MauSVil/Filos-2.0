2157211d6d32ffa8cbcc911c7a5d9d33dbfd021f
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2025-03-29 15:33:07 -0600

    sign-in

--- app/api/sign-in/route.ts
+++ app/api/sign-in/route.ts
@@ -1,20 +1,39 @@
+import { UsersRepository } from "@/repositories/users.repository";
 import ky from "ky";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const Body = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+})
+
+type Body = z.infer<typeof Body>;
 
 export const POST = async (req: NextRequest) => {
-  try {
-    const body = (await req.json()) as Body;
-    // TODO: api-filos remove
-    const resp = await ky.post("https://api-filos.mausvil.dev/auth/login", {
-      json: body,
-    });
-    const json: { data: { token: string }; error: string } = await resp.json();
+  let validatedBody: Body;
 
-    if (json.error) throw new Error(json.error);
-    (await cookies()).set("token", json?.data?.token, {
-      maxAge: 60 * 60 * 24 * 7,
-    });
+  try {
+    validatedBody = Body.parse(await req.json());
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error al iniciar sesion" },
+      { status: 400 },
+    );
+  }
+
+  try {
+    console.log({ validatedBody });
+    const userFound = await UsersRepository.findOne({ email: validatedBody.email });
+    console.log({ userFound });
+
+    // const json: { data: { token: string }; error: string } = await resp.json();
+
+    // if (json.error) throw new Error(json.error);
+    // (await cookies()).set("token", json?.data?.token, {
+    //   maxAge: 60 * 60 * 24 * 7,
+    // });
 
     return NextResponse.json({ message: "Se inicio sesion correctamente" });
   } catch (error) {

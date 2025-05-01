1cb36d457d3c6358445aa26f44f77163e689c505
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2025-05-01 14:21:32 -0600

    buyer type fixes

--- types/v2/Buyer/Base.type.ts
+++ types/v2/Buyer/Base.type.ts
@@ -1,9 +1,10 @@
+import { ObjectId } from "mongodb";
 import { z } from "zod";
 
 export const BuyerBase = z.object({
   name: z.string(),
   email: z.string().email().optional(),
-  phone: z.string(),
+  phone: z.coerce.string(),
   address: z.string(),
   isChain: z.boolean().default(false),
   createdAt: z.date().optional(),
@@ -15,5 +16,7 @@ export type BuyerBaseType = z.infer<typeof BuyerBase>;
 export const BuyerInput = BuyerBase;
 export type BuyerInputType = z.infer<typeof BuyerInput>;
 
-export const BuyerUpdate = BuyerBase.partial();
+export const BuyerUpdate = BuyerBase.extend({
+  _id: z.instanceof(ObjectId),
+}).partial();
 export type BuyerUpdateType = z.infer<typeof BuyerUpdate>;

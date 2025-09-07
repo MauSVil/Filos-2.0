8cb8cc58b9e7223a475fd202ead5b9200fefb180
Mauricio <maudev96@gmail.com>
2025-09-07 17:07:04 -0600

    feat: improve layout structure in Home component for better responsiveness

--- app/(private)/page.tsx
+++ app/(private)/page.tsx
@@ -8,12 +8,18 @@ import { SectionCards } from "@/components/section-cards";
 const Home = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-6 gap-4">
-      <ProductsOutOfStock />
-      <SalesPerMonth />
-      <div className="col-span-6">
+      <div className="col-span-1 md:col-span-3">
+        <ProductsOutOfStock />
+      </div>
+      <div className="col-span-1 md:col-span-3">
+        <SalesPerMonth />
+      </div>
+      <div className="col-span-1 md:col-span-6">
         <SectionCards />
       </div>
-      <HistoryMovements />
+      <div className="col-span-1 md:col-span-6">
+        <HistoryMovements />
+      </div>
     </div>
   );
 };

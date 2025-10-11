2c3a5ee8ff1f82322aff6e60fc6a3aa97593142b
Mau S Vilchis <maudev96@gmail.com>
2025-10-11 16:57:19 -0600

    feat: Add GET endpoint for retrieving contacts by ID

--- app/api/v2/contacts/[id]/route.ts
+++ app/api/v2/contacts/[id]/route.ts
@@ -1,6 +1,25 @@
 import { ContactRepository } from "@/repositories/v2/ContacRepository";
 import { NextRequest, NextResponse } from "next/server";
 
+
+export const GET = async (
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) => {
+  try {
+    const { id } = await params;
+    const contact = await ContactRepository.findOne({ _id: id });
+
+    if (!contact) {
+      return NextResponse.json({ message: "Contacto no encontrado" }, { status: 404 });
+    }
+
+    return NextResponse.json(contact, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error interno del servidor" }, { status: 500 });
+  }
+}
+
 export const PUT = async (
   request: NextRequest,
   { params }: { params: Promise<{ id: string }> }
